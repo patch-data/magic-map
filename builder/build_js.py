c0da5c41ab28bf41f5d061fe3d3fe47202f8396f
Martin Burch <cburch@journalism.cuny.edu>
2013-07-26 13:17:57 -0400

    Added the JSON builder code

---
+++ builder/build_js.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Reads a CSV file and outputs a JavaScript object
+
+# Pass it a CSV file
+
+import sys
+import csv
+import json
+from decimal import Decimal
+
+# Override the dumb JSON encoder
+# Thanks to http://stackoverflow.com/questions/8652497/caught-typeerror-while-rendering-decimal51-8-is-not-json-serializable
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        super(DecimalEncoder, self).default(o)
+
+wanted_fields = ['Subdomain','Lat','Lng']
+
+filename =  sys.argv[1]
+
+full_dict = csv.DictReader(open(filename, 'rU'))
+
+trimmed_dict = {}
+
+for row in full_dict:
+	trimmed_dict[row['Subdomain']] = [Decimal(row['Lat']),Decimal(row['Lng'])]
+	
+	
+with open('patches.json', 'w') as outfile:
+	json.dump(trimmed_dict,outfile,cls=DecimalEncoder)
